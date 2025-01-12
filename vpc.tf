6b2d95eb55fdd6d1ba6c34dc8533307dc743ce02
aalimsee <aaron.limse@hotmail.com>
2025-01-13 02:23:54 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,9 @@
+resource "aws_vpc" "aalimsee_tf_main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "aalimsee-tf-vpc"
+  }
+}
