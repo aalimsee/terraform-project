14d93eacc82b665ceb21faa5d5f8ef1bc9668200
aalimsee <aaron.limse@hotmail.com>
2025-01-13 01:50:25 +0800

    Update outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.aalimsee_tf_main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.aalimsee_tf_public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.aalimsee_tf_private[*].id
+}
