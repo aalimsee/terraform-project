de4768fba2fbc196616c67f0dce10af8b24ae754
aalimsee <aaron.limse@hotmail.com>
2025-01-13 01:51:31 +0800

    Update subnets.tf

---
+++ networking/subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "aalimsee_tf_public" {
+  count                   = 3
+  vpc_id                  = aws_vpc.aalimsee_tf_main.id
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "aalimsee-tf-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "aalimsee_tf_private" {
+  count             = 3
+  vpc_id            = aws_vpc.aalimsee_tf_main.id
+  cidr_block        = "10.0.${count.index + 4}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "aalimsee-tf-private-subnet-${count.index + 1}"
+  }
+}
