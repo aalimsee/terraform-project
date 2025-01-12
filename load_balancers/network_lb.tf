b98ec90ad3d6d0cb22e5acb851cad89e2c6e9750
aalimsee <aaron.limse@hotmail.com>
2025-01-13 02:07:42 +0800

    Update network_lb.tf

--- load_balancers/network_lb.tf
+++ load_balancers/network_lb.tf
@@ -1,3 +1,41 @@
-resource "aws_lb" "aalimsee_tf_db_nlb" { ... }
-resource "aws_lb_target_group" "aalimsee_tf_db_tg" { ... }
-resource "aws_lb_listener" "aalimsee_tf_db_listener" { ... }
+resource "aws_lb" "aalimsee_tf_db_nlb" { 
+  name               = "aalimsee-tf-db-nlb"
+  internal           = true # Internal NLB for private communication
+  load_balancer_type = "network"
+  subnets            = aws_subnet.aalimsee_tf_private[*].id
+
+  tags = {
+    Name = "aalimsee-tf-db-nlb"
+  }
+}
+
+resource "aws_lb_target_group" "aalimsee_tf_db_tg" { 
+  name        = "aalimsee-tf-db-target-group"
+  port        = 3306 # Database port (MySQL)
+  protocol    = "TCP" # NLB uses TCP for DB traffic
+  vpc_id      = aws_vpc.aalimsee_tf_main.id
+
+  health_check {
+    protocol            = "TCP" # Health check uses TCP
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "aalimsee-tf-db-tg"
+  }
+}
+
+resource "aws_lb_listener" "aalimsee_tf_db_listener" { 
+  load_balancer_arn = aws_lb.aalimsee_tf_db_nlb.arn
+  port              = 3306
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.aalimsee_tf_db_tg.arn
+  }
+}
+
