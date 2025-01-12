6e0005eba5c0a06e78780216f541817976dbeca4
aalimsee <aaron.limse@hotmail.com>
2025-01-13 02:14:40 +0800

    Update web_asg.tf

--- compute/web_asg.tf
+++ compute/web_asg.tf
@@ -34,4 +34,30 @@ EOF
   }
 }
 
-resource "aws_autoscaling_group" "aalimsee_tf_web_asg" { ... }
+resource "aws_autoscaling_group" "aalimsee_tf_web_asg" { 
+  name    = "aalimsee-tf-web-asg" # Add this line to define the ASG name
+  
+  launch_template {
+    id      = aws_launch_template.aalimsee_tf_web_asg_lt.id
+    version = "$Latest"
+  }
+
+  min_size            = 2 
+  max_size            = 5
+  desired_capacity    = 2
+  vpc_zone_identifier = aws_subnet.aalimsee_tf_public[*].id
+
+  target_group_arns = [
+    aws_lb_target_group.aalimsee_tf_tg.arn
+  ]
+
+  tag {
+    key                 = "Name"
+    value               = "aalimsee-tf-web-asg"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
